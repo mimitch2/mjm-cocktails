536c75c8b713d1582f3231f663195b6f311f922b
mimitch2 <mike@melodix.com>
2018-06-23 13:28:45 -0500

    finally fixed all the material-ui issues

--- client/src/components/SearchFeild.js
+++ client/src/components/SearchFeild.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import Autosuggest from 'react-autosuggest';
 import match from 'autosuggest-highlight/match';
 import parse from 'autosuggest-highlight/parse';
-import TextField from 'material-ui/TextField';
-import Paper from 'material-ui/Paper';
-import { MenuItem } from 'material-ui/Menu';
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import { MenuItem } from '@material-ui/core/Menu';
 import "../css/App.css"
 import { withStyles } from '@material-ui/core/styles';
 
--- client/src/components/TabBar.js
+++ client/src/components/TabBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-import "../css/App.css"
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import SwipeableViews from 'react-swipeable-views';
-import AppBar from 'material-ui/AppBar';
-import Tabs, { Tab } from 'material-ui/Tabs';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import FavoritesGrid from "../containers/FavoritesGridContainer.js"
 import Badge from '@material-ui/core/Badge';
@@ -92,15 +92,15 @@ class FullWidthTabs extends React.Component {
           index={this.state.value}
           onChangeIndex={this.handleChangeIndex}>
                 
-          <TabContainer dir={theme.direction}>{this.props.controls}{this.props.drinkView}</TabContainer>
+          <TabContainer dir={theme.direction}>{this.props.controls}{this.props.drinkView}</TabContainer> 
 
           {/* *********** Try putting Link around  this container******** */}
           <TabContainer dir={theme.direction}>
-            <FavoritesGrid /* favorites={this.props.favorites} */
+            <FavoritesGrid 
               returnedDrink={this.props.returnedDrink}
               addRemoveFav={this.props.addRemoveFav} 
               getDrink={this.props.getDrink} 
-              switchTab={this.switchTabsOnFavClick}/>
+              switchTab={this.switchTabsOnFavClick}/> 
           </TabContainer>
 
           <TabContainer dir={theme.direction}><SubmitForm fullData={this.props.fullData}/></TabContainer>
@@ -120,3 +120,110 @@ class FullWidthTabs extends React.Component {
 // };
 
 export default withStyles(styles, { withTheme: true })(FullWidthTabs);
+
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { withStyles } from '@material-ui/core/styles';
+// import SwipeableViews from 'react-swipeable-views';
+// import AppBar from '@material-ui/core/AppBar';
+// import Tabs from '@material-ui/core/Tabs';
+// import Tab from '@material-ui/core/Tab';
+// import Typography from '@material-ui/core/Typography';
+// import FavoritesGrid from "../containers/FavoritesGridContainer.js"
+// import Badge from '@material-ui/core/Badge';
+// import SubmitForm from './SubmitForm';
+
+// function TabContainer({ children, dir }) {
+//   return (
+//     <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+//       {children}
+//     </Typography>
+//   );
+// }
+
+// TabContainer.propTypes = {
+//   children: PropTypes.node.isRequired,
+//   dir: PropTypes.string.isRequired,
+// };
+
+// const styles = theme => ({
+//   root: {
+//     backgroundColor: theme.palette.background.paper,
+//     width: 500,
+//   },
+// });
+
+// class FullWidthTabs extends React.Component {
+//   state = {
+//     value: 0,
+//   };
+
+//   handleChange = (event, value) => {
+//     this.setState({ value });
+//   };
+
+//   handleChangeIndex = index => {
+//     this.setState({ value: index });
+//   };
+
+//   render() {
+//     const { classes, theme } = this.props;
+
+//     return (
+//       <div className={classes.root}>
+//         <AppBar position="sticky" color="default" style={{backgroundColor: 'rgb(60, 60, 60)', color: "white"}}>
+
+//           <span style={{ fontSize: "24px", marginTop: "8px", marginLeft: "10px", fontWeight: "500"}}>I NEED A DRINK</span> 
+
+//           <div className="icon" style={{marginBottom: "-40px", marginTop: "6px", marginLeft:"10px", display: "flex", alignItems: "center"}}>
+//             <img src="/img/MartiniGlass.png" alt="" height="55px" width="55px" />
+//           </div>
+
+
+//           <Tabs
+//             value={this.state.value}
+//             onChange={this.handleChange}
+//             indicatorColor="primary"
+//             textColor="inherit"
+//             centered>
+
+//             <Tab label="Find Drinks" />
+           
+//             <Tab label={ 
+//               <Badge className={classes.padding} color="primary" badgeContent={this.props.favorites.length} >Favorites
+//               </Badge>
+//             }/>
+
+//             <Tab label="Add Drinks" />
+            
+//             <Tab label="Info" />
+
+//           </Tabs>
+//         </AppBar>
+//         <SwipeableViews
+//           axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+//           index={this.state.value}
+//           onChangeIndex={this.handleChangeIndex}>
+                
+//           {<TabContainer dir={theme.direction}>{this.props.controls}{this.props.drinkView}</TabContainer> }
+//           <TabContainer dir={theme.direction}>
+//             <FavoritesGrid 
+//               returnedDrink={this.props.returnedDrink}
+//               addRemoveFav={this.props.addRemoveFav} 
+//               getDrink={this.props.getDrink} 
+//               switchTab={this.switchTabsOnFavClick}/>
+//           </TabContainer>
+//           <TabContainer dir={theme.direction}><SubmitForm fullData={this.props.fullData}/></TabContainer>
+//         </SwipeableViews>
+//       </div>
+//     );
+//   }
+// }
+
+// FullWidthTabs.propTypes = {
+//   classes: PropTypes.object.isRequired,
+//   theme: PropTypes.object.isRequired,
+// };
+
+// export default withStyles(styles, { withTheme: true })(FullWidthTabs);
